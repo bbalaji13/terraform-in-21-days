ab5e833ba6e129b2b5b3681bc8b7d54e084b91db
balaji borra <bbalaji13@gmail.com>
2023-05-29 15:54:02 +0530

    use count and length

--- vpc.tf
+++ vpc.tf
@@ -1,11 +1,11 @@
 locals {
-  public_cidr = ["10.0.0.0/24","10.0.1.0/24"]
-  private_cidr =[ "10.0.101.0/24","10.0.102.0/24"]
-  availability_zones = ["us-west-2a","us-west-2b"]
+  public_cidr        = ["10.0.0.0/24", "10.0.1.0/24"]
+  private_cidr       = ["10.0.101.0/24", "10.0.102.0/24"]
+  availability_zones = ["us-west-2a", "us-west-2b"]
 }
 
 resource "aws_vpc" "main" {
-  cidr_block       = "10.0.0.0/16"
+  cidr_block = "10.0.0.0/16"
 
   tags = {
     Name = "main"
@@ -15,8 +15,8 @@ resource "aws_vpc" "main" {
 resource "aws_subnet" "public" {
   count = length(local.public_cidr)
 
-  vpc_id     = aws_vpc.main.id
-  cidr_block =  local.public_cidr[count.index]
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = local.public_cidr[count.index]
   availability_zone = local.availability_zones[count.index]
 
   tags = {
@@ -27,8 +27,8 @@ resource "aws_subnet" "public" {
 resource "aws_subnet" "private" {
   count = length(local.private_cidr)
 
-  vpc_id     = aws_vpc.main.id
-  cidr_block = local.private_cidr[count.index]
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = local.private_cidr[count.index]
   availability_zone = local.availability_zones[count.index]
 
   tags = {
@@ -59,18 +59,18 @@ resource "aws_route_table" "public" {
 }
 
 resource "aws_route_table_association" "public" {
-  count = length(local.public_cidr)
+  count          = length(local.public_cidr)
   subnet_id      = aws_subnet.public[count.index].id
   route_table_id = aws_route_table.public.id
 }
 
 resource "aws_eip" "nat" {
   count = length((local.private_cidr))
-  vpc      = true
+  vpc   = true
 }
 
 resource "aws_nat_gateway" "nat" {
-  count = length(local.public_cidr)
+  count         = length(local.public_cidr)
   allocation_id = aws_eip.nat[count.index].id
   subnet_id     = aws_subnet.public[count.index].id
 
@@ -82,18 +82,18 @@ resource "aws_nat_gateway" "nat" {
 
 
 resource "aws_route_table" "private" {
-  count = length(local.private_cidr)
+  count  = length(local.private_cidr)
   vpc_id = aws_vpc.main.id
 
   route {
-    cidr_block = "0.0.0.0/0"
+    cidr_block     = "0.0.0.0/0"
     nat_gateway_id = aws_nat_gateway.nat[count.index].id
   }
 }
 
 
 resource "aws_route_table_association" "private" {
-  count = 2
+  count          = 2
   subnet_id      = aws_subnet.private[count.index].id
   route_table_id = aws_route_table.private[count.index].id
 }
