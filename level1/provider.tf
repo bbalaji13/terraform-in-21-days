a8fb48dfc4a7b9a4229121634b5676b86292d2b6
balaji borra <bbalaji13@gmail.com>
2023-06-01 00:04:17 +0530

    created levels

---
+++ level1/provider.tf
@@ -0,0 +1,13 @@
+terraform {
+  backend "s3" {
+    bucket         = "balaji-my-tf-test-bucket"
+    key            = "level1.tfstate"
+    region         = "us-west-2"
+    dynamodb_table = "balaji-terraform-remote-state"
+
+  }
+}
+
+provider "aws" {
+  region = "us-west-2"
+}
