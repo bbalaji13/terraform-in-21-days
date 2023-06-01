4068e2ea21ea45200b1a88826f1bed32da7d8e56
balaji borra <bbalaji13@gmail.com>
2023-06-01 13:09:17 +0530

    load balancer

--- level2/userdata.sh
+++ level2/userdata.sh
@@ -8,7 +8,9 @@ sudo a2enmod rewrite
 sudo a2enmod headers
 sudo a2enmod ssl
 
-git clone https://github.com/gabrielecirulli/2048.git
-sudo cp -R 2048/* /var/www/html
+# git clone https://github.com/gabrielecirulli/2048.git
+# sudo cp -R 2048/* /var/www/html
+
+echo "Hello from instance $(hostname)" > /var/www/html/index.html
 
 sudo service apache2 restart
--- level2/variables.tf
+++ level2/variables.tf
@@ -1,3 +1,3 @@
 variable "env_code" {
-    default = "mumbai"
+  default = "mumbai"
 }
