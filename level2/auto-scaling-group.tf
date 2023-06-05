17400b3840611464ef0cd3dc30da7c8004703f34
balaji borra <bbalaji13@gmail.com>
2023-06-05 22:53:07 +0530

    Modules created

--- level2/auto-scaling-group.tf
+++ level2/auto-scaling-group.tf
@@ -18,10 +18,10 @@ data "aws_ami" "ubuntu" {
 module "asg" {
   source = "../modules/asg"
 
-  env_code          = "mumbai"
-  ami_id            = data.aws_ami.ubuntu.id
-  target_group_arn = module.lb.target_group_arn
-  private_subnet_id = data.terraform_remote_state.level1.outputs.private_subnet_id
+  env_code             = "mumbai"
+  ami_id               = data.aws_ami.ubuntu.id
+  target_group_arn     = module.lb.target_group_arn
+  private_subnet_id    = data.terraform_remote_state.level1.outputs.private_subnet_id
   lb_security_group_id = module.lb.lb_security_group_id
-  vpc_id = data.terraform_remote_state.level1.outputs.vpc_id
+  vpc_id               = data.terraform_remote_state.level1.outputs.vpc_id
 }
